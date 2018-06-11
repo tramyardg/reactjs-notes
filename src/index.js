8f4201bf62b4b25cf1868492fcc6ae465c5b701d
Raymart De Guzman <tramyardg@gmail.com>
2018-06-10 20:21:41 -0400

    displaying current state

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './css/index.css';
 import App from './App';
+import MyData from './tutorial/myData';
 import ExampleData from './tutorial/exampleData';
 import Button from './tutorial/defaultProps';
 
@@ -10,27 +11,9 @@ import registerServiceWorker from './registerServiceWorker';
 const rootDiv = document.getElementById('root');
 ReactDOM.render(<App />, rootDiv);
 
-const headings = ['When', 'Who', 'Description'];
-const data = [
-    {
-        "when": "2 minutes ago",
-        "who": "Jill Dupre",
-        "description": "Created new account"
-    },
-    {
-        "when": "1 hour ago",
-        "who": "Lose White",
-        "description": "Added fist chapter"
-    },
-    {
-        "when": "2 hours ago",
-        "who": "Jordan Whash",
-        "description": "Created new account"
-    }
-];
 const dataContainer = document.getElementById('example-data');
 ReactDOM.render(
-    <ExampleData headings={headings} dataSet={data} />,
+    <ExampleData headings={MyData.headingData} dataSet={MyData.rowData}/>,
     dataContainer
 );
 
@@ -42,7 +25,3 @@ ReactDOM.render(
 );
 
 registerServiceWorker();
-
-
-// the app first points here
-// then reads the import
---
+++ src/tutorial/state.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {title: 'Best App'};
+  }
+
+  render() {
+    return (
+        <h1>{this.state.title}</h1>
+    );
+  }
+}
