b4557364a0b0ca87a85cbf0cb7f5dcb8922060f9
Raymart De Guzman <tramyardg@gmail.com>
2018-06-15 22:02:31 -0400

    GuineaPigs.js written as stateless functional components

--- src/tutorial/soc/components/GuineaPigs.js
+++ src/tutorial/soc/components/GuineaPigs.js
@@ -1,14 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 // Its only job is to render HTML-like JSX.
-export class GuineaPigs extends Component {
-  render () {
-    let src = this.props.src; // property pass to this component class
-    return (
+// export class GuineaPigs extends Component {
+//   render () {
+//     let src = this.props.src; // property pass to this component class
+//     return (
+//       <div>
+//         <h1>Cute Guinea Pigs</h1>
+//         <img src={src} alt="Guinea pigs pics"/>
+//       </div>
+//     );
+//   }
+// }
+
+// written as stateless functional components
+export const GuineaPigs = (props) => {
+  return (
       <div>
         <h1>Cute Guinea Pigs</h1>
-        <img src={src} alt="Guinea pigs pics"/>
+        <img src={props.src} alt="Guinea pigs pics"/>
       </div>
-    );
-  }
-}
+  );
+};
